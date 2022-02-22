b1e561d4b78db70c7a7585541264b516b7771069
s-todorova <siyana.todorova01@gmail.com>
2022-02-22 23:00:43 +0200

    react base

---
+++ fmi-biseri/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="z420">
+      <h1> Nqma nishto oshte :(</h1>
+    </div>
+  );
+}
+
+export default App;
